cb7439af7f9deea0a5f4d90efb1f0a6d561abaad
Pavel Semkin <pvsemk@mail.ru>
2019-05-18 14:26:00 +0300

    Update config.py and ssd.py

--- data/config.py
+++ data/config.py
@@ -13,8 +13,10 @@ MEANS = (104, 117, 123)
 # SSD300 CONFIGS
 voc = {
     'num_classes': 48,
-    'lr_steps': (80000, 100000, 120000),
-    'max_iter': 120000,
+    #'lr_steps': (80000, 100000, 120000),
+    'lr_steps':(20000, 30000, 40000),
+    #'max_iter': 120000,
+    'max_iter': 40000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
